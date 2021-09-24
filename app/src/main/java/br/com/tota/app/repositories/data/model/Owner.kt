217d065a813b571d73af2d5ce87986c6b2602f06
Antonio Moura <antonio.gmoura@gmail.com>
2021-09-24 14:02:42 -0400

    Aula 8 - Finalização do APP

--- app/src/main/java/br/com/tota/app/repositories/data/model/Owner.kt
+++ app/src/main/java/br/com/tota/app/repositories/data/model/Owner.kt
@@ -1,6 +1,9 @@
 package br.com.tota.app.repositories.data.model
 
+import com.google.gson.annotations.SerializedName
+
 data class Owner(
     val login: String,
+    @SerializedName("avatar_url")
     val avatarURL: String
 )
--- app/src/main/java/br/com/tota/app/repositories/data/repositories/RepoRepositoryImpl.kt
+++ app/src/main/java/br/com/tota/app/repositories/data/repositories/RepoRepositoryImpl.kt
@@ -1,12 +1,18 @@
 package br.com.tota.app.repositories.data.repositories
 
+import br.com.tota.app.repositories.core.RemoteException
 import br.com.tota.app.repositories.data.services.GitHubService
 import kotlinx.coroutines.flow.flow
+import retrofit2.HttpException
 
 class RepoRepositoryImpl(private val service: GitHubService) : RepoRepository {
 
     override suspend fun listRepositories(user: String) = flow {
-        val repoList = service.listRepositories(user)
-        emit(repoList)
+        try {
+            val repoList = service.listRepositories(user)
+            emit(repoList)
+        } catch (ex: HttpException) {
+            throw RemoteException(ex.message ?: "Não foi possivel fazer a busca no momento!")
+        }
     }
 }
