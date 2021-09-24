4cbb8604548d7485b96109774a3e4c3486d4407a
Antonio Moura <antonio.gmoura@gmail.com>
2021-09-23 22:09:10 -0400

    Aula 5 - Criação do Data Module

---
+++ app/src/main/java/br/com/tota/app/repositories/data/services/GitHubService.kt
@@ -0,0 +1,9 @@
+package br.com.tota.app.repositories.data.services
+
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface GitHubService {
+    @GET("users/{user}/repos")
+    suspend fun listRepositories(@Path("user") user: String)
+}
